64468f948270074f8fd80c2de06b73d43515f243
vishnuroxx <183851233+vishnuroxx@users.noreply.github.com>
2026-09-03 11:38:04 +0800

    Implement JavaFx-Tut 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.Region;
 
 /**
  * 
@@ -10,12 +17,66 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    // Image Links
+    private Image userImage = new Image(this.getClass()
+            .getResourceAsStream("/images/DaUser.jpg"));
+
+    private Image dukeImage = new Image(
+            this.getClass().getResourceAsStream("/images/DaDuke.jpg"));
+
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World"); // Create label
-        Scene scene = new Scene(helloWorld); // Set the label as scene
-        stage.setScene(scene); // Set the stage to show the scene
-        stage.show(); // Render the stage
+        // Setting up required components
+
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        DialogBox dialogBox = new DialogBox("Hello", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        userInput.setPrefWidth(325.0);
+        userInput.setPrefHeight(30);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
     }
 
 }
